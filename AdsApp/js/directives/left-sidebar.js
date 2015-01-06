45e63b1460158ca81358099db8bb5db1be960bf7
d-kibarev <kibarev@abv.bg>
2015-01-06 22:00:25 +0200

    beginning

---
+++ AdsApp/js/directives/left-sidebar.js
@@ -0,0 +1,8 @@
+﻿app.directive('leftSidebar', function () {
+    return {
+        controller: 'HomeCtrl',
+        restrict: 'E',
+        templateUrl: 'templates/directives/left-sidebar.html',
+        replace: true
+    };
+});
