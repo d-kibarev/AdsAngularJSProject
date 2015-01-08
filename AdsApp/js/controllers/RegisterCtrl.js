3338815440387bd4cc609e7c764e0246cc165c50
d-kibarev <kibarev@abv.bg>
2015-01-08 15:24:18 +0200

    Some CSS adjustments

--- AdsApp/js/controllers/RegisterCtrl.js
+++ AdsApp/js/controllers/RegisterCtrl.js
@@ -1,9 +1,14 @@
 ﻿'use strict';
 app.controller('RegisterCtrl', ['$scope', 'towns', function ($scope, towns) {
     $scope.headerTitle = 'Register';
+
     towns.getTowns()
     .$promise
     .then(function (data) {
         $scope.towns = data;
     });
+
+    $scope.register = function (user) {
+        console.log(user);
+    }
 }]);
