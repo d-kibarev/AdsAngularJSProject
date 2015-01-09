74169cb1d4b51fee54bc8b2d79de3b448fdd35d0
d-kibarev <kibarev@abv.bg>
2015-01-09 15:31:41 +0200

    login and noty are successful.

--- AdsApp/js/controllers/LoginCtrl.js
+++ AdsApp/js/controllers/LoginCtrl.js
@@ -1,4 +1,15 @@
 ﻿'use strict';
-app.controller('LoginCtrl', ['$scope', function ($scope) {
+app.controller('LoginCtrl', ['$scope', '$location', 'authService', 'notyService', function ($scope, $location, authService, notyService) {
     $scope.headerTitle = 'Login';
+    $scope.login = function (user) {
+        authService.login(user,
+            function success() {
+                notyService.showInfo("Login successful");
+                $location.path("/");
+            },
+            function error(err) {
+                notyService.showError("Login failed", err);
+            }
+        );
+    };
 }]);
