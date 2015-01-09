d36c6a5de222d6e0ac1f9555d227a08bfb51baa8
d-kibarev <kibarev@abv.bg>
2015-01-09 20:00:21 +0200

    Show logged user and make logout

---
+++ AdsApp/js/controllers/AppCtrl.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+app.controller('AppCtrl',
+    function ($scope, $location, authService, notyService) {
+        $scope.authService = authService;
+
+        $scope.logout = function () {
+            authService.logout();
+            notyService.showInfo("Logout successful");
+            $location.path('/');
+        };
+    }
+);
