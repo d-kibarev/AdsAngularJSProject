693dd493a2fca2e30412c9afc70710479ae20a72
d-kibarev <kibarev@abv.bg>
2015-01-09 13:54:17 +0200

    Add loadin bar.

--- AdsApp/js/controllers/PublicAdsCtrl.js
+++ AdsApp/js/controllers/PublicAdsCtrl.js
@@ -1,7 +1,9 @@
 ﻿app.controller('PublicAdsCtrl', ['$scope', 'adService', function myfunction($scope, adService) {
+    $scope.isReady = false;
     adService.getAllAds()
     .$promise
     .then(function (data) {
         $scope.adsData = data;
+        $scope.isReady = true;
     });
 }]);
